4ffffa9d8d4a688e4f46f0b4359f300f1bea1bdc
yashpanwar1050 <yashpanwar1050@gmail.com>
2026-09-13 23:00:09 +0530

    Add files via upload

---
+++ ques11.c
@@ -0,0 +1,16 @@
+/*Write a program to input an integer and check whether it is even or odd using conditional statement*/
+#include <stdio.h>
+int main() {
+    int number;
+
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    if (number % 2 == 0) {
+        printf("%d is even.\n", number);
+    } else {
+        printf("%d is odd.\n", number);
+    }
+
+    return 0;
+}
