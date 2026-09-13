ee46f2328979185c407e36ca5cbb0cb4e7070354
yashpanwar1050 <yashpanwar1050@gmail.com>
2026-09-13 22:55:55 +0530

    Add files via upload

---
+++ ques6.c
@@ -0,0 +1,20 @@
+/*Write a program to swap two numbers using a third temporary variable*/
+
+#include <stdio.h>
+
+int main() {
+    int a, b, temp;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Before swapping: a = %d, b = %d\n", a, b);
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    printf("After swapping: a = %d, b = %d\n", a, b);
+
+    return 0;
+}
