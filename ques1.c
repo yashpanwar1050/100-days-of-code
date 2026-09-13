1b954745e847c77225e44ea601a72e5408d6b369
yashpanwar1050 <yashpanwar1050@gmail.com>
2026-09-13 22:02:47 +0530

    Add files via upload

---
+++ ques1.c
@@ -0,0 +1,10 @@
+//program to input two numbers and display their sum
+#include <stdio.h>
+int main() {
+    int a,b;
+    printf("enter two integers");
+    scanf("%d %d",&a,&b);
+    int sum=a+b;
+    printf("sum = %d",sum);
+    return 0;
+}
